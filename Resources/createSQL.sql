627e2fcabe4f63ff5b1db292b2c19546f4bae529
Sumover <test@runnoob.com>
2019-07-15 11:13:19 +0800

    add deleteValues note

---
+++ Resources/createSQL.sql
@@ -0,0 +1,46 @@
+-- 用户表创建
+create table user_data
+(
+  id            int auto_increment
+    primary key,
+  name          varchar(20) not null,
+  password      varchar(20) not null,
+  register_date date        not null,
+  type          varchar(20) not null
+);
+-- 添加第一个超级用户
+insert into user_data (name, password, register_date, type)
+values (`sumover`, `2323180`, current_date, `teacher`);
+
+-- 问题表创建
+create table questions
+(
+  id           int auto_increment
+    primary key,
+  score        int         not null,
+  type         varchar(20) not null,
+  id_in_others int         not null
+);
+
+-- 选择题表创建
+create table multiple_choise_questions
+(
+  id         int auto_increment
+    primary key,
+  `describe` text not null comment '描述',
+  options    text not null comment '选项, 用json表示'
+)
+  comment '选择题的一个题库';
+
+-- 试卷表的创建
+create table examination_paper
+(
+  id            int auto_increment
+    primary key,
+  name          varchar(100) not null,
+  create_date   date         not null,
+  note          text         null,
+  question_list text         not null comment '题目id表,字符串类型,两个id之间用空格分开'
+)
+  comment '试卷';
+
--- src/main/Module/ExaminaionPaper.java
+++ src/main/Module/ExaminaionPaper.java
@@ -1,6 +1,9 @@
 package main.Module;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ExaminaionPaper {
@@ -70,4 +73,35 @@ public class ExaminaionPaper {
     public void setNote(String note) {
         this.note = note;
     }
+
+
+    /**
+     * @param questionList 被解析的问题id列表， 直接从数据库里爬的
+     * @return 解析完的数组
+     */
+    public static int[] parseQuestionIdList(String questionList) {
+        List<Integer> list = new LinkedList<Integer>();
+        int id = 0;
+        for (int i = 0; i < questionList.length(); ++i) {
+            char c = questionList.charAt(i);
+            if (Character.isDigit(c)) {
+                id = id * 10 + c - '0';
+            } else {
+                list.add(id);
+            }
+        }
+        list.add(id);
+        int[] arr = new int[list.size()];
+        for (int i = 0; i < arr.length; ++i)
+            arr[i] = list.get(i);
+        return arr;
+    }
+
+    public static String parseQuestionListToIdList(List<Question> questionList) {
+        String parse = Integer.toString(questionList.get(0).getId());
+        for (int i = 1; i < questionList.size(); ++i)
+            parse += " " + Integer.toString(questionList.get(i).getId());
+        return parse;
+    }
+
 }
