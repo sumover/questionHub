ed4a1e4e8b1bf4ea83b7fa4938e3e9027c4b6a50
Sumover <test@runnoob.com>
2019-07-16 20:48:20 +0800

    add some page

--- src/main/servletDispatcher/InitDispatcher.java
+++ src/main/servletDispatcher/InitDispatcher.java
@@ -3,6 +3,7 @@ package main.servletDispatcher;
 import main.Module.MessageBoard;
 import main.SQLConnctor.Connector;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
